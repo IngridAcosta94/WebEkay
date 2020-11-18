478225401d8d51da38b9567e19e03d640dd49e4d
Jesus Dionicio <misterioso2212@gmail.com>
2020-11-17 18:18:35 -0600

    Piedra sobre piedra

--- EKay/Startup.cs
+++ EKay/Startup.cs
@@ -24,6 +24,7 @@ namespace EKay
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ EKay/wwwroot/js/test.js
@@ -0,0 +1,16 @@
+﻿const url = "https://localhost:44321/api/empresa/1"
+//https://pokeapi.co/api/v2/pokemon
+fetch(url,
+    {
+        mode: "no-cors",
+    }
+)
+.then(response => response.json())
+.then(data => {
+   let test = document.getElementById("test")
+   test.innerHTML = `<p>${data.nombre}</p>`;
+
+   console.log(data)
+})
+.catch(err => console.log(err))
+    
