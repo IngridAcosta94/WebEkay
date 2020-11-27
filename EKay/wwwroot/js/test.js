45e87e011560abe9e1c51e7dc2983c8e5a88fe06
Jesus Dionicio <misterioso2212@gmail.com>
2020-11-27 12:38:19 -0600

    Integrando Api Fetch - 1

--- EKay/wwwroot/js/test.js
+++ EKay/wwwroot/js/test.js
@@ -1,16 +1,20 @@
-﻿const url = "https://localhost:44321/api/empresa/1"
-//https://pokeapi.co/api/v2/pokemon
-fetch(url,
-    {
-        mode: "no-cors",
-    }
-)
-.then(response => response.json())
-.then(data => {
-   let test = document.getElementById("test")
-   test.innerHTML = `<p>${data.nombre}</p>`;
+﻿const url = "https://localhost:44321/api/empresa/"
+const test = "https://pokeapi.co/api/v2/pokemon/1"
+var Test = document.querySelector('#test')
+fetch(url)
+    .then(res => res.json()) //get
+    .then(data => {
+        console.log(data)  //mostrar en la consola
 
-   console.log(data)
-})
-.catch(err => console.log(err))
+        //mostrar en la pagina
+        Test.innerHTML = `
+            <p>${data.data[0].nombre}</p >
+            <p>${data.data[0].correo}</p >
+            <p>${data.data[0].direccion}</p >
+            <p>${data.data[0].id}</p >
+            <p>${data.data[0].nombreRepresentante}</p >
+            <p>${data.data[0].telefono}</p >
+        `
+
+    })
     
