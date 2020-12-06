ae0875998f26f1d3c9a37dab238b3e957d1aa1b6
Jesus Dionicio <misterioso2212@gmail.com>
2020-12-06 14:16:59 -0600

    Sin ganas parte 1

--- EKay/Pages/Inicio.cshtml.cs
+++ EKay/Pages/Inicio.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ekay.Clases;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,9 @@ namespace EKay.Pages
 {
     public class InicioModel : PageModel
     {
+        [BindProperty]
+        public DocumentoRequestDto DocumentoRequest { get; set; }
+
         public void OnGet()
         {
         }
